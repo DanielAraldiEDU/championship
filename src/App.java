4c726c94881789c8b58af633d163e6b2693ffa5a
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-05-13 15:04:50 -0300

    feat: making composition on Application

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.util.Date;
+
 public class App {
   public static void main(String[] args) throws Exception {
-    
+    Team team = new Team(0, "Mercedes");
+
+    Racetrack racetrack = new Racetrack("Interlagos");
+
+    Organizer organizer = new Organizer("FIA");
+
+    int championshipIndex = organizer.createChampionship(2023, "F1");
+    organizer.subscribe(team, championshipIndex);
+    organizer.addRace(racetrack, new Date(), championshipIndex);
   }
 }
